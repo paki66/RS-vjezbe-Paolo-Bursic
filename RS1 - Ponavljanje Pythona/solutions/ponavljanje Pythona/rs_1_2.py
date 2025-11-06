728e8fd297ee7004b537ebac0cc4a0041a5234f7
pbursic <paolo6.bursic6@gmail.com>
2025-11-06 09:59:01 +0100

    RS-1 - zadatak 2 uspješno rješen

---
+++ RS1 - Ponavljanje Pythona/solutions/ponavljanje Pythona/rs_1_2.py
@@ -0,0 +1,16 @@
+def count_vowels_consonants(tekst):
+    output = {"vowels": 0, "consonants": 0}
+    vowels = "aeiouAEIOU"
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+
+    for i in tekst:
+        if i in vowels:
+            output["vowels"] += 1
+        if i in consonants:
+            output["consonants"] += 1
+
+    return output
+
+tekst = "Python je programski jezik koji je jednostavan za učenje i korištenje. Python je vrlo popularan."
+
+print(count_vowels_consonants(tekst))
