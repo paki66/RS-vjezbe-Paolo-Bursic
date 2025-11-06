61365ff9ca4ac118de2c3a29d8fceb8c66eea847
pbursic <paolo6.bursic6@gmail.com>
2025-11-06 17:06:47 +0100

    RS-1 - Nino je dobija broj

---
+++ RS1 - Ponavljanje Pythona/solutions/Nino Telefonino/main/phone_number_book.py
@@ -0,0 +1,167 @@
+phone_number_book = {
+    "01": {
+        "mjesto": "Grad Zagreb i Zagrebačka županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "020": {
+        "mjesto": "Dubrovačko-neretvanska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "021": {
+        "mjesto": "Splitsko-dalmatinska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "022": {
+        "mjesto": "Šibensko-kninska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "023": {
+        "mjesto": "Zadarska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "031": {
+        "mjesto": "Osječko-baranjska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "032": {
+        "mjesto": "Vukovarsko-srijemska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "033": {
+        "mjesto": "Virovitičko-podravska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "034": {
+        "mjesto": "Požeško-slavonska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "035": {
+        "mjesto": "Brodsko-posavska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "040": {
+        "mjesto": "Međimurska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "042": {
+        "mjesto": "Varaždinska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "043": {
+        "mjesto": "Bjelovarsko-bilogorska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "044": {
+        "mjesto": "Sisačko-moslavačka županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "047": {
+        "mjesto": "Karlovačka županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "048": {
+        "mjesto": "Koprivničko-križevačka županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "049": {
+        "mjesto": "Krapinsko-zagorska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "051": {
+        "mjesto": "Primorsko-goranska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "052": {
+        "mjesto": "Istarska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "053": {
+        "mjesto": "Ličko-senjska županija",
+        "operater": None,
+        "vrsta": "fiksna mreža"
+    },
+    "091": {
+        "mjesto": None,
+        "operater": "A1 Hrvatska",
+        "vrsta": "mobilna mreža"
+    },
+    "092": {
+        "mjesto": None,
+        "operater": "Tomato",
+        "vrsta": "mobilna mreža"
+    },
+    "095": {
+        "mjesto": None,
+        "operater": "Telemach",
+        "vrsta": "mobilna mreža"
+    },
+    "097": {
+        "mjesto": None,
+        "operater": "bonbon",
+        "vrsta": "mobilna mreža"
+    },
+    "098": {
+        "mjesto": None,
+        "operater": "Hrvatski Telekom",
+        "vrsta": "mobilna mreža"
+    },
+    "099": {
+        "mjesto": None,
+        "operater": "Hrvatski Telekom",
+        "vrsta": "mobilna mreža"
+    },
+    "0800": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+    "060": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+    "061": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+    "064": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+    "065": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+    "069": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+    "072": {
+        "mjesto": None,
+        "operater": None,
+        "vrsta": "posebne usluge"
+    },
+}
