08d3c053cbf25181bad5a182848564622d1d20b8
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 11:09:56 +0100

    RS-2 - 1_2 zadatak rjesen

---
+++ RS2 - Napredniji Python koncepti/solutions/1-Lambda-izrazi/z2_zbroji_kvadriraj.py
@@ -0,0 +1,11 @@
+"""
+Napiši lambda fukciju:
+def zbroji_pa_kvadriraj(a, b):
+    return (a + b) ** 2
+"""
+
+zbroji_pa_kvadriraj = lambda a, b : (a + b) ** 2
+
+a, b = 10, 2
+
+print(f"zbroji_pa_kvadriraj{a, b} = {zbroji_pa_kvadriraj(a, b)}")
