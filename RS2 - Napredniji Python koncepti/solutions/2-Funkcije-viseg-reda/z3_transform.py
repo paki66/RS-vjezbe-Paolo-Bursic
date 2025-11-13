0e3077b8b0e5f502d2f506c298cf668432899b9c
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 12:18:03 +0100

    RS-2 - (z2_3) transform rjesen

--- RS2 - Napredniji Python koncepti/solutions/2-Funkcije-viseg-reda/z3_transform.py
+++ RS2 - Napredniji Python koncepti/solutions/2-Funkcije-viseg-reda/z3_transform.py
@@ -1,10 +1,11 @@
 """
-Koristeći odgovarajuću funkciju višeg reda i lambda izraz (bez comprehensiona), pohranite u varijablu transform rezultat kvadriranja svih brojeva u listi gdje rezultat mora biti rječnik gdje su ključevi originalni brojevi, a vrijednosti kvadrati tih brojeva:
+Koristeći odgovarajuću funkciju višeg reda i lambda izraz (bez comprehensiona), pohranite u varijablu transform
+rezultat kvadriranja svih brojeva u listi gdje rezultat mora biti rječnik gdje su ključevi originalni brojevi,
+a vrijednosti kvadrati tih brojeva:
+"""
 
 brojevi = [10, 5, 12, 15, 20]
 
-transform = ...
+transform = dict(map(lambda broj : (broj, broj ** 2), brojevi))
 
 print(transform) # {10: 100, 5: 25, 12: 144, 15: 225, 20: 400}
-"""
-
