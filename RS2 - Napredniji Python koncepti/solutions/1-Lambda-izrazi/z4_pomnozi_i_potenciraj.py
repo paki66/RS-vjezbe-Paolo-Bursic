89c896d99dc785a375639a75fd747ca874bee26c
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 11:21:48 +0100

    RS-2 - (z1_4) pomnozi_i_potenciraj rješen

---
+++ RS2 - Napredniji Python koncepti/solutions/1-Lambda-izrazi/z4_pomnozi_i_potenciraj.py
@@ -0,0 +1,12 @@
+"""
+Pomnoži vrijednost s 5 pa potenciraj na x:
+
+def pomnozi_i_potenciraj(x, y):
+    return (y * 5) ** x
+"""
+
+pomnozi_i_potenciraj = lambda x, y : (y * 5) ** x
+
+eksponent, faktor = 2, 8
+
+print(f"pomnozi_i_potenciraj{eksponent, faktor} = {pomnozi_i_potenciraj(eksponent, faktor)}")
