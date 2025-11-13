8b48751c5aa53cd96c90af919363c5e92048a0a1
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 12:05:34 +0100

    RS-2 - (z2_2) filter_vecih_od_5 rjesen

--- RS2 - Napredniji Python koncepti/solutions/2-Funkcije-viseg-reda/z2_filter_vecih_od_5.py
+++ RS2 - Napredniji Python koncepti/solutions/2-Funkcije-viseg-reda/z2_filter_vecih_od_5.py
@@ -1,10 +1,10 @@
 """
 Koristeći funkciju filter, filtrirajte samo brojeve koji su veći od 5:
+"""
 
 brojevi = [1, 21, 33, 45, 2, 2, 1, -32, 9, 10]
 
-veci_od_5 = ...
+veci_od_5 = list(filter(lambda broj : broj > 5, brojevi))
 
 print(veci_od_5) # [21, 33, 45, 9, 10]
-"""
 
