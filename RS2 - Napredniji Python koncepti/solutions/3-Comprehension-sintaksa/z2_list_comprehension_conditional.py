b1540c87e8e4d95e9a5c0032ac14e4d4ccb72887
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 13:12:47 +0100

    RS-2 - (z3_2) list_comprehension_conditional rjesen

--- RS2 - Napredniji Python koncepti/solutions/3-Comprehension-sintaksa/z2_list_comprehension_conditional.py
+++ RS2 - Napredniji Python koncepti/solutions/3-Comprehension-sintaksa/z2_list_comprehension_conditional.py
@@ -5,6 +5,6 @@ ali samo za nizove koji sadrže slovo a:
 
 rijeci = ["jabuka", "pas", "knjiga", "zvijezda", "prijatelj", "zvuk", "čokolada", "ples", "pjesma", "otorinolaringolog"]
 
-duljine_sa_slovom_a = ...
+duljine_sa_slovom_a = [len(rijec) for rijec in rijeci if "a" in rijec]
 
 print(duljine_sa_slovom_a) # [6, 3, 6, 8, 9, 8, 6, 17]
