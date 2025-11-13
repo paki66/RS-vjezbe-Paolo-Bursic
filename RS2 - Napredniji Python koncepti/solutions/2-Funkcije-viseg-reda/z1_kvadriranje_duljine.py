88211f6966c55c8751f830f5e5f95a10e10fd64f
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 11:42:22 +0100

    RS-2 - (z2) pronti za rješit

---
+++ RS2 - Napredniji Python koncepti/solutions/2-Funkcije-viseg-reda/z1_kvadriranje_duljine.py
@@ -0,0 +1,10 @@
+"""
+Koristeći funkciju map, kvadrirajte duljine svih nizova u listi:
+
+nizovi = ["jabuka", "kruška", "banana", "naranča"]
+
+kvadrirane_duljine = ...
+
+print(kvadrirane_duljine) # [36, 36, 36, 49]
+"""
+
---
+++ RS2 - Napredniji Python koncepti/solutions/2-Funkcije-viseg-reda/z4_punoljetni_studenti.py
@@ -0,0 +1,17 @@
+"""
+Koristeći funkcije all i map, provjerite jesu li svi studenti punoljetni:
+
+studenti = [
+    {"ime": "Ivan", "prezime": "Ivić", "godine": 19},
+    {"ime": "Marko", "prezime": "Marković", "godine": 22},
+    {"ime": "Ana", "prezime": "Anić", "godine": 21},
+    {"ime": "Petra", "prezime": "Petrić", "godine": 13},
+    {"ime": "Iva", "prezime": "Ivić", "godine": 17},
+    {"ime": "Mate", "prezime": "Matić", "godine": 18}
+]
+
+svi_punoljetni = ...
+
+print(svi_punoljetni) # False
+"""
+
