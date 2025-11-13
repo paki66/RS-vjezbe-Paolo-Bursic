5dc218441ca7e1ebaa0d5cd6730c67e2e0b9c40d
paolo <paolo.bursic@student.unipu.hr>
2025-11-13 13:21:54 +0100

    RS-2 - (z3_4) dictionary_comprehension rjesen

--- RS2 - Napredniji Python koncepti/solutions/3-Comprehension-sintaksa/z4_dictionary_comprehension.py
+++ RS2 - Napredniji Python koncepti/solutions/3-Comprehension-sintaksa/z4_dictionary_comprehension.py
@@ -3,7 +3,11 @@ Koristeći dictionary comprehension, izgradite rječnik iteriranjem kroz listu b
 ključevi brojevi, a vrijednosti su korijeni tih brojeva zaokruženi na 2 decimale:
 """
 
-korijeni = ...
+from math import sqrt
+
+korjenuj = lambda x : round(sqrt(x), 2)
+
+korijeni = {broj: korjenuj(broj) for broj in range(50, 501, 50)}
 
 print(korijeni) # {50: 7.07, 100: 10.0, 150: 12.25, 200: 14.14, 250: 15.81, 300: 17.32, \
                 #  350: 18.71, 400: 20.0, 450: 21.21, 500: 22.36}
