9ccc63fcd0f2225580259b5e61005fa0cf7a5181
paolo <paolo.bursic@student.unipu.hr>
2025-11-16 16:31:39 +0100

    RS-3 - zadatak 1 rjesen

--- RS3 - Osnove asyncio biblioteke/solutions/z1_korutina.py
+++ RS3 - Osnove asyncio biblioteke/solutions/z1_korutina.py
@@ -5,3 +5,14 @@ Listu brojeva definirajte comprehensionom.
 Nakon isteka vremena, u korutinu ispišite poruku "Podaci dohvaćeni." i vratite podatke.
 Riješite bez korištenja asyncio.gather() i asyncio.create_task() funkcija.
 """
+
+import asyncio
+
+async def get_numbers():
+    numbers = [num for num in range(1, 11)]
+    print(numbers)
+    await asyncio.sleep(3)
+    print("Podaci dohvaćeni.")
+    return numbers
+
+asyncio.run(get_numbers())
