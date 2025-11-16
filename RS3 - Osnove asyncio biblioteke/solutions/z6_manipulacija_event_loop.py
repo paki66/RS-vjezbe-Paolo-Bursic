8f2b24a00ea1292608efa2957fdb7ae99237293f
paolo <paolo.bursic@student.unipu.hr>
2025-11-16 18:40:43 +0100

    RS-3 - zadatak 6 postavljen

--- RS3 - Osnove asyncio biblioteke/solutions/z6_manipulacija_event_loop.py
+++ RS3 - Osnove asyncio biblioteke/solutions/z6_manipulacija_event_loop.py
@@ -27,3 +27,26 @@ t2 = time.perf_counter()
 print(results)
 print(f"Vrijeme izvođenja {t2 - t1:.2f} sekunde")
 """
+
+import asyncio, time
+
+async def fetch_data(param):
+    print(f"Nešto radim s {param}...")
+    await asyncio.sleep(param)
+    print(f'Dovršio sam s {param}.')
+    return f"Rezultat za {param}"
+
+async def main():
+    task1 = asyncio.create_task(fetch_data(1)) # schedule
+    task2 = asyncio.create_task(fetch_data(2)) #schedule
+
+    result1 = await task1
+    print("Fetch 1 uspješno završen.")
+    return [result1]
+
+
+t1 = time.perf_counter()
+results = asyncio.run(main()) # pokretanje event loop-a
+t2 = time.perf_counter()
+print(results)
+print(f"Vrijeme izvođenja {t2 - t1:.2f} sekunde")
