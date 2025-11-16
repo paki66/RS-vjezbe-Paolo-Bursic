43d29ccf2fbc3b4852cec8171d6887812975f3fe
paolo <paolo.bursic@student.unipu.hr>
2025-11-16 17:05:22 +0100

    RS-3 - zadatak 3 postavljen

--- RS3 - Osnove asyncio biblioteke/solutions/z3_korutina_autentifikacija.py
+++ RS3 - Osnove asyncio biblioteke/solutions/z3_korutina_autentifikacija.py
@@ -29,3 +29,35 @@ baza_lozinka = [
   {'korisnicko_ime': 'maja_0x', 'lozinka': 's324SDFfdsj234'},
   {'korisnicko_ime': 'zdeslav032', 'lozinka': 'deso123'}
 ]
+
+autorizirani_korisnik = {
+    "korisnicko_ime": "mirko123",
+    "email": "mirko123@gmail.com",
+    "lozinka": "lozinka123"
+}
+
+krivi_unos_lozinke = {
+    "korisnicko_ime": "maja_0x",
+    "email": "majaaaaa@gmail.com",
+    "lozinka": "lozinka123"
+}
+
+nepostojeci_korisnik = {
+    "korisnicko_ime": "ana_anic",
+    "email": "majaaaaa@gmail.com",
+    "lozinka": "lozinka123"
+}
+
+import asyncio
+
+async def authenticate(korisnik):
+    pass
+
+
+async def authorize():
+    pass
+
+asyncio.run(authenticate(autorizirani_korisnik))
+# asyncio.run(authenticate(krivi_unos_lozinke))
+# asyncio.run(authenticate(nepostojeci_korisnik))
+
