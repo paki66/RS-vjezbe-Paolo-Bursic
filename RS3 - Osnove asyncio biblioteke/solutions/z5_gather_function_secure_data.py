effe32d3dd0a961287692c30de2b8afffd4de0d6
paolo <paolo.bursic@student.unipu.hr>
2025-11-16 18:37:54 +0100

    RS-3 - zadatak 5 rjesen

--- RS3 - Osnove asyncio biblioteke/solutions/z5_gather_function_secure_data.py
+++ RS3 - Osnove asyncio biblioteke/solutions/z5_gather_function_secure_data.py
@@ -8,3 +8,31 @@ Korutina secure_data mora za svaki rječnik vratiti novi rječnik u obliku:
     {'prezime':prezime, 'broj_kartice': 'enkriptirano', 'CVV': 'enkriptirano'}.
 Za fake enkripciju koristite funkciju hash(str) koja samo vraća hash vrijednost ulaznog stringa.
 """
+
+unsecure_data_list = [
+    {"surname": "Buršić", "card_number": "A ča ćeš?", "CVV": "123"},
+    {"surname": "Terlević", "card_number": "Rješiti bug!", "CVV": "345"},
+    {"surname": "Pliško", "card_number": "A ča drugo!XD", "CVV": "567"},
+]
+
+import asyncio
+
+async def secure_data(unsecure_data):
+    print(f"Securing data for {unsecure_data['surname']}...")
+    await asyncio.sleep(3)
+
+    secured_data = {
+        "surname": unsecure_data["surname"],
+        "card_number": hash(unsecure_data["card_number"]),
+        "CVV": hash(unsecure_data["CVV"])
+    }
+
+    return print(secured_data)
+
+
+async def main():
+    tasks = list(map(lambda data : asyncio.create_task(secure_data(data)), unsecure_data_list))
+    await asyncio.gather(*tasks)
+
+
+asyncio.run(main())
