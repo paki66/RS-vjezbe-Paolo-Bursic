8cc2a135e07e72c0835fa6c880c31b3df2a7d9f6
paolo <paolo.bursic@student.unipu.hr>
2025-11-16 18:00:04 +0100

    RS-3 - zadatak 4 započet

--- RS3 - Osnove asyncio biblioteke/solutions/z4_koristenje_gather_funkcije.py
+++ RS3 - Osnove asyncio biblioteke/solutions/z4_koristenje_gather_funkcije.py
@@ -1,5 +1,5 @@
 """
-Definirajte korutinu provjeri_parnostkoja će simulirati "super zahtjevnu operaciju" provjere parnosti broja putem vanjskog API-ja.
+Definirajte korutinu provjeri_parnost koja će simulirati "super zahtjevnu operaciju" provjere parnosti broja putem vanjskog API-ja.
 Korutina prima kao argument broj za koji treba provjeriti parnost, a vraća poruku "Broj {broj} je paran." ili
     "Broj {broj} je neparan." nakon 2 sekunde.
 Unutar main funkcije definirajte listu 10 nasumičnih brojeva u rasponu od 1 do 100 (koristite random modul).
@@ -8,3 +8,21 @@ Nakon toga, pohranite u listu zadaci 10 Task objekata koji će izvršavati korut
     svaki broj iz liste (također kroz list comprehension).
 Na kraju, koristeći asyncio.gather(), pokrenite sve korutine konkurentno i ispišite rezultate.
 """
+
+import asyncio, random
+
+async def provjeri_parnost():
+    paran, broj = False, 1
+
+    if paran:
+        return print(f"Broj {broj} je paran.")
+    else:
+        return print(f"Broj {broj} nije paran.")
+
+
+async def main():
+    pass
+
+
+asyncio.run(main())
+asyncio.run(provjeri_parnost())  # TODO testing
